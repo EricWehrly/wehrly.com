fc219dc32522832652b27e4eda02beb90e56e9df
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-08-12 22:38:48 -0400

    fully remove lookbook module

--- main.tf
+++ main.tf
@@ -35,7 +35,3 @@ module "wedding-site" {
   certificate_arn = "arn:aws:acm:us-east-1:428933486948:certificate/67a70258-5001-4be7-8225-ed3540c746df"
   # certificate_arn = "arn:aws:acm:us-east-1:428933486948:certificate/45fd1980-e04c-4848-a5f4-d883e258d364"
 }
-
-module "lookbook-site" {
-  source = "./modules/dummy"
-}
